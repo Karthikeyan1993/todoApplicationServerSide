5d29d148f51cdc210f1152673f65ee585e129fd2
angular-bull <karthik.infotec99@gmail.com>
2020-06-03 15:55:47 +0530

    Update Scheduler.java

--- src/main/java/com/app/todo/automation/Scheduler.java
+++ src/main/java/com/app/todo/automation/Scheduler.java
@@ -37,8 +37,9 @@ public class Scheduler {
     }
 
 //     @Scheduled(cron = "0 0 0 * * *", zone = "Indian/Maldives")
-//     @Scheduled(cron = "0 0 0/3 * * ?")
-    @Scheduled(cron = "0 0/2 * * * ?")
+    
+//     @Scheduled(cron = "0 0/2 * * * ?")
+    @Scheduled(cron = "0 0 0/6 * * ?")
     public void cornJob() throws Exception {
         LOGGER.info("Job Started At {}", new Date());
         process(this.todoRepository.findAll());
